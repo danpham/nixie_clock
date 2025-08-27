59400442dc757e00019e9fe4609e89720d3cc217
Dan Pham <dan@danpham.com>
2025-08-27 16:21:40 +0200

    Add pinout mapping. Update display software.

--- software/nixie_clock_esp32_project/components/display/display.c
+++ software/nixie_clock_esp32_project/components/display/display.c
@@ -23,18 +23,47 @@
 /******************************************************************
  * 5. Functions prototypes (static only)
 ******************************************************************/
-static uint64_t encode_time(uint8_t hours, uint8_t minutes, uint8_t seconds);
-
+static uint64_t encode_time(uint8_t hours, uint8_t minutes, uint8_t seconds, uint8_t dot1, uint8_t dot2, uint8_t nixie3_dot, uint8_t nixie6_dot);
+ 
 
 /******************************************************************
  * 6. Functions definitions
 ******************************************************************/
-static uint64_t encode_time(uint8_t hours, uint8_t minutes, uint8_t seconds) {
+static uint8_t shift_compute(uint8_t number){
+    uint8_t shift_number = 0;
+
+    if  (number > 0)
+    {
+       shift_number = 10 - number;
+    }
+
+    return number;
+}
+
+static uint64_t encode_time(uint8_t hours, uint8_t minutes, uint8_t seconds, uint8_t dot1, uint8_t dot2, uint8_t nixie3_dot, uint8_t nixie6_dot) {
     uint64_t data = 0;
-    // TODO: encode according to Nixie wiring
-    data |= ((uint64_t)hours & 0x3F) << 48;
-    data |= ((uint64_t)minutes & 0x3F) << 24;
-    data |= ((uint64_t)seconds & 0x3F);
+    uint8_t nixies[6];
+
+    data |= ((uint64_t)dot1 & 0x01) << 11;
+    data |= ((uint64_t)dot2 & 0x01) << 53;
+    data |= ((uint64_t)nixie3_dot & 0x01);
+    data |= ((uint64_t)nixie6_dot & 0x01) << 32;
+
+    // Split
+    nixies[0] = hours / 10;
+    nixies[1] = hours % 10;
+    nixies[2] = minutes / 10;
+    nixies[3] = minutes % 10;
+    nixies[4] = seconds / 10;
+    nixies[5] = seconds % 10;
+
+    data |= (uint64_t)0x01 << shift_compute(nixies[0]) << 22; // hour hi
+    data |= (uint64_t)0x01 << shift_compute(nixies[1]) << 12; // hour low   
+    data |= (uint64_t)0x01 << shift_compute(nixies[2]) << 1; // minute hi
+    data |= (uint64_t)0x01 << shift_compute(nixies[3]) << 54; // minute lo
+    data |= (uint64_t)0x01 << shift_compute(nixies[4]) << 43; // second hi
+    data |= (uint64_t)0x01 << shift_compute(nixies[5]) << 33; // second low
+
     return data;
 }
 
@@ -43,7 +72,7 @@ void display_init(void) {
 }
 
 void display_set_time(uint8_t hours, uint8_t minutes, uint8_t seconds) {
-    uint64_t data = encode_time(hours, minutes, seconds);
+    uint64_t data = encode_time(hours, minutes, seconds, 1, 1, 0, 0);
     hv5622_send64(data);
 }
 
