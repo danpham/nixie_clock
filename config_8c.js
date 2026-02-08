c5725e7b59f9f2b5855aa3660bf4a144d8807763
embarquech <embarquech@users.noreply.github.com>
2026-02-08 10:50:08 +0000

    deploy: 9d25e77f5829be5a3d4d6ad164bfe79ab3e599c9

--- config_8c.js
+++ config_8c.js
@@ -2,7 +2,6 @@ var config_8c =
 [
     [ "config_get_copy", "config_8c.html#a69d38a23f87686ff93103dd50d30f0f6", null ],
     [ "config_init", "config_8c.html#ace6647db1f20678900580f60562dea57", null ],
-    [ "config_read", "config_8c.html#a0fb5d5c5b55b283acc39104003047fc2", null ],
     [ "config_save", "config_8c.html#adbf182647ef90017c070ed7a4d0504fb", null ],
     [ "config_set_config", "config_8c.html#ab33362128ad31589907741e41a579530", null ],
     [ "config_mutex", "config_8c.html#a7db430aae1d4f3683e19ed50e7e57996", null ],
